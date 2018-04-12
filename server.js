588e69cf77895c34d2b9ca450ae836f859e9bca0
gotenks-1 <Sharad.Tomer@GRAPECITY.NET>
2018-04-12 16:23:24 +0530

    first

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/',(req, res)=>{
+    res.json({
+        'message':'first server'
+    });
+});
+
+app.listen(8000,()=>{
+    console.log('active at 8000');
+})
